0b29c95f3189ddd3162ee23646426a487c72ea9d
Prakhar Gupta <prakhar897@gmail.com>
2018-03-23 13:52:40 +0530

    fd

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
 var x = document.getElementById("demo");
-var lat,lon;
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
+var url;
+function start()
+{
+	if (navigator.geolocation) 
+	{
+        navigator.geolocation.getCurrentPosition(function(position)
+        {
+        	var ourRequest = new XMLHttpRequest();
+        	url ="http://api.openweathermap.org/data/2.5/weather?lat="+parseInt(position.coords.latitude)+"&lon="+parseInt(position.coords.longitude)+"&id=524901&APPID=33bf83187c4b7768f5ece4c332470a70";
+        	ourRequest.open('GET',url);
+        	ourRequest.onload = function()
+        	{
+				var data = JSON.parse(ourRequest.responseText);
+				console.log(data);
+			}
+			ourRequest.send();
+        });
+    } 
+    else 
+    { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
-}
-
-function showPosition(position) {
-    lat =  parseInt(position.coords.latitude);
-    lon =  parseInt(position.coords.longitude);
-    console.log(lat+" "+lon);
-}
-
-function start(){
-	getLocation();
-	var ourRequest = new XMLHttpRequest();
-	var url ="http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&id=524901&APPID=33bf83187c4b7768f5ece4c332470a70";
-	ourRequest.open('GET',url);
-	ourRequest.onload = function(){
-	var data = ourRequest.responseText;
-	console.log(data);
-	}
-	ourRequest.send();
 }
 
 
